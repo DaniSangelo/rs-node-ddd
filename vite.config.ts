6528a3f8f1d10ce85d1e7eb3639762f6eab05b6c
Daniel Sângelo <danielsangelo@hotmail.com>
2024-12-14 09:06:33 -0300

    Fix path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     globals: true, // Ativa o uso de globais no Vitest
     environment: 'node', // Configura o ambiente de teste
