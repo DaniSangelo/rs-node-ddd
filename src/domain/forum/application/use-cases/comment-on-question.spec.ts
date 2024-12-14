0035d49847abf9d5e678dcaada24d5674d61c5b8
Daniel Sângelo <danielsangelo@hotmail.com>
2024-12-14 12:56:35 -0300

    Create comment on question use case

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,36 @@
+import { CommentOnQuestionUseCase } from './comment-on-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let sut: CommentOnQuestionUseCase
+
+describe('Comment on Question use case', () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    )
+  })
+
+  it('should comment on question', async () => {
+    const question = makeQuestion()
+    await inMemoryQuestionsRepository.create(question)
+
+    await sut.execute({
+      questionId: question.id.toString(),
+      authorId: question.id.toString(),
+      content: 'Test Comment',
+    })
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toEqual(
+      'Test Comment',
+    )
+  })
+})
